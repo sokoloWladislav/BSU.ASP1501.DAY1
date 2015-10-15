3bda8542d8aba03a241ea1899099a9ad3889ba1a
Uladzislau_Sakalou <needyoursoul2die@gmail.com>
2015-10-15 22:56:45 +0300

    added Newton's method

---
+++ Logic/Solver.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic
+{
+    public class Solver
+    {
+        public double MethodNewton(double num, double power, double fault)
+        {
+            double result = (power - 1 + num) / power;
+
+            if (power == 0)
+                result = 1;
+
+            else if (num < 0 && power % 2 == 0)
+                result = Double.NaN;
+
+            else if (num == 0)
+                result = 0;
+
+            else
+            {
+                while (Math.Abs(RaiseInPower(result, power) - num) >= fault)
+                    result = ((power - 1) * result + num / RaiseInPower(result, power - 1)) / power;
+            }
+            return result;
+        }
+
+        private double RaiseInPower(double num, double power)
+        {
+            double result = num;
+            for (int i = 0; i < power - 1; ++i)
+                result *= num;
+            return result;
+        }
+    }
+}
---
+++ LogicTests/SolverTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Logic;
+
+namespace LogicTests
+{
+    [TestClass]
+    public class SolverTest
+    {
+        [TestMethod]
+        public void TestMethodNewton()
+        {
+            Solver solver = new Solver();
+            double power = 3;
+            double num = 27;
+            double fault = 0.1;
+
+            double actual = solver.MethodNewton(num, power, fault);
+            double expected = Math.Pow(actual, power);
+
+            Assert.AreEqual(num, expected, 0.1);
+
+            num = -27;
+
+            actual = solver.MethodNewton(num, power, fault);
+            expected = Math.Pow(actual, power);
+
+            Assert.AreEqual(num, expected, 0.1);
+
+            actual = solver.MethodNewton(1, 4, fault);
+
+            Assert.AreEqual(1, actual, fault);
+
+            power = 0;
+
+            Assert.AreEqual(1, solver.MethodNewton(num, power, fault));
+
+            power = 2;
+
+            actual = solver.MethodNewton(num, 2, fault);
+            Assert.IsTrue(Double.IsNaN(actual));
+        }
+
+    }
+}
