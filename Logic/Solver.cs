cef0d85c42d196558af32d4eb59d69ea87ad138b
Uladzislau_Sakalou <needyoursoul2die@gmail.com>
2015-10-17 21:04:20 +0300

    added Euclide and Stein's methods

--- Logic/Solver.cs
+++ Logic/Solver.cs
@@ -8,7 +8,8 @@ namespace Logic
 {
     public class Solver
     {
-        public double MethodNewton(double num, double power, double fault)
+        #region Newton's Method
+        public static double MethodNewton(double num, double power, double fault)
         {
             double result = (power - 1 + num) / power;
 
@@ -29,12 +30,177 @@ namespace Logic
             return result;
         }
 
-        private double RaiseInPower(double num, double power)
+        public static int EuclideMethod(int a, int b)
+        {
+            GetAbs(ref a, ref b);
+            CheckOnOrder(ref a, ref b);
+            int result = a;
+            if (b != 0)
+            {
+                if (a % b == 0)
+                {
+                    result = Math.Abs(b);
+                }
+                else
+                    result = EuclideMethod(b, a % b);
+            }
+            return result;
+        } 
+        #endregion
+
+        #region Euclide's Method
+        public static int EuclideMethod(out long time, int a, int b)
+        {
+            var stopWatch = new System.Diagnostics.Stopwatch();
+            stopWatch.Start();
+            int result = EuclideMethod(a, b);
+            stopWatch.Stop();
+            time = stopWatch.Elapsed.Ticks;
+            return result;
+        }
+
+        public static int EuclideMethod(int a, int b, int c)
+        {
+            int result = EuclideMethod(a, b);
+            result = EuclideMethod(result, c);
+            return result;
+        }
+
+        public static int EuclideMethod(out long time, int a, int b, int c)
+        {
+            var stopWatch = new System.Diagnostics.Stopwatch();
+            stopWatch.Start();
+            int result = EuclideMethod(a, b, c);
+            stopWatch.Stop();
+            time = stopWatch.Elapsed.Ticks;
+            return result;
+        }
+
+        public static int EuclideMethod(params int[] data)
+        {
+            int result = data[0];
+            for (int i = 1; i < data.Length; ++i)
+                result = EuclideMethod(data[i], result);
+            return result;
+        }
+
+        public static int EuclideMethod(out long time, params int[] data)
+        {
+            var stopWatch = new System.Diagnostics.Stopwatch();
+            int result = data[0];
+            for (int i = 1; i < data.Length; ++i)
+            {
+                stopWatch.Start();
+                result = EuclideMethod(data[i], result);
+                stopWatch.Stop();
+            }
+            time = stopWatch.Elapsed.Ticks;
+            return result;
+        } 
+        #endregion
+
+        #region Sten's Method
+        public static int SteinMethod(int a, int b)
+        {
+            GetAbs(ref a, ref b);
+            CheckOnOrder(ref a, ref b);
+            int result = a;
+            if (a != b && b != 1 && b != 0)
+            {
+                if (a % 2 == 0 && b % 2 == 0)
+                    result = 2 * SteinMethod(a / 2, b / 2);
+                if (a % 2 == 0 && b % 2 == 1)
+                    result = SteinMethod(a / 2, b);
+                if (a % 2 == 1 && b % 2 == 0)
+                    result = SteinMethod(a, b / 2);
+                if (a % 2 == 1 && b % 2 == 1)
+                    result = SteinMethod((a - b) / 2, b);
+            }
+            if (b == 1)
+                result = 1;
+            return result;
+        }
+
+        public static int SteinMethod(out long time, int a, int b)
+        {
+            var stopWatch = new System.Diagnostics.Stopwatch();
+            stopWatch.Start();
+            int result = SteinMethod(a, b);
+            stopWatch.Stop();
+            time = stopWatch.Elapsed.Ticks;
+            return result;
+        }
+
+        public static int SteinMethod(int a, int b, int c)
+        {
+            int result = SteinMethod(a, b);
+            result = SteinMethod(result, c);
+            return result;
+        }
+
+        public static int SteinMethod(out long time, int a, int b, int c)
+        {
+
+            var stopWatch = new System.Diagnostics.Stopwatch();
+            stopWatch.Start();
+            int result = SteinMethod(a, b, c);
+            stopWatch.Stop();
+            time = stopWatch.Elapsed.Ticks;
+            return result;
+        }
+
+
+        public static int SteinMethod(params int[] data)
+        {
+            int result = data[0];
+            for (int i = 1; i < data.Length; ++i)
+                result = EuclideMethod(data[i], result);
+            return result;
+        }
+
+        public static int SteinMethod(out long time, params int[] data)
+        {
+            var stopWatch = new System.Diagnostics.Stopwatch();
+            int result = data[0];
+            for (int i = 1; i < data.Length; ++i)
+            {
+                stopWatch.Start();
+                result = EuclideMethod(data[i], result);
+                stopWatch.Stop();
+            }
+            time = stopWatch.Elapsed.Ticks;
+            return result;
+        }
+        #endregion
+
+        #region Private Methods
+        private static double RaiseInPower(double num, double power)
         {
             double result = num;
             for (int i = 0; i < power - 1; ++i)
                 result *= num;
             return result;
         }
+
+        private static void Swap(ref int a, ref int b)
+        {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+
+        private static void CheckOnOrder(ref int a, ref int b)
+        {
+            if (a < b)
+                Swap(ref a, ref b);
+        }
+
+        private static void GetAbs(ref int a, ref int b)
+        {
+            a = Math.Abs(a);
+            b = Math.Abs(b);
+        }
+        #endregion
+
     }
 }
--- LogicTests/SolverTest.cs
+++ LogicTests/SolverTest.cs
@@ -7,39 +7,346 @@ namespace LogicTests
     [TestClass]
     public class SolverTest
     {
+        #region Test Newton's Method
         [TestMethod]
-        public void TestMethodNewton()
+        public void Test_MethodNewton_PositiveNumbers()
         {
-            Solver solver = new Solver();
             double power = 3;
             double num = 27;
             double fault = 0.1;
 
-            double actual = solver.MethodNewton(num, power, fault);
+            double actual = Solver.MethodNewton(num, power, fault);
             double expected = Math.Pow(actual, power);
 
-            Assert.AreEqual(num, expected, 0.1);
-
-            num = -27;
-
-            actual = solver.MethodNewton(num, power, fault);
-            expected = Math.Pow(actual, power);
-
-            Assert.AreEqual(num, expected, 0.1);
-
-            actual = solver.MethodNewton(1, 4, fault);
-
-            Assert.AreEqual(1, actual, fault);
-
-            power = 0;
-
-            Assert.AreEqual(1, solver.MethodNewton(num, power, fault));
-
-            power = 2;
-
-            actual = solver.MethodNewton(num, 2, fault);
-            Assert.IsTrue(Double.IsNaN(actual));
+            Assert.AreEqual(expected, num, 0.1);
         }
 
+        [TestMethod]
+        public void Test_MethodNewton_NegativeNumbers()
+        {
+            double power = 3;
+            double num = -27;
+            double fault = 0.1;
+
+            double actual = Solver.MethodNewton(num, power, fault);
+            double expected = Math.Pow(actual, power);
+
+            Assert.AreEqual(expected, num, 0.1);
+        }
+
+        [TestMethod]
+        public void Test_MethodNewton_Power0Returned1()
+        {
+            double power = 0;
+            double num = 4;
+            double fault = 0.1;
+
+            double actual = Solver.MethodNewton(num, power, fault);
+            double expected = 1;
+
+            Assert.AreEqual(expected, actual, 0.1);
+        }
+
+        [TestMethod]
+        public void Test_MethodNewton_Num1Returned1()
+        {
+            double power = 4;
+            double num = 1;
+            double fault = 0.1;
+
+            double actual = Solver.MethodNewton(num, power, fault);
+            double expected = 1;
+
+            Assert.AreEqual(expected, num, 0.1);
+        }
+
+        [TestMethod]
+        public void Test_MethodNewton_NegativeNumPower2ReturnedNaN()
+        {
+            double power = 2;
+            double num = -27;
+            double fault = 0.1;
+
+            double actual = Solver.MethodNewton(num, power, fault);
+
+            Assert.IsTrue(Double.IsNaN(actual));
+        } 
+        #endregion
+
+        #region Test Euclide's Method
+        [TestMethod]
+        public void Test_EuclideMethod_PositiveNums()
+        {
+            int a = 8;
+            int b = 12;
+            int expected = 4;
+
+            int actual = Solver.EuclideMethod(a, b);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Test_EuclideMethod_NegativeNums()
+        {
+            int a = -8;
+            int b = -12;
+            int expected = 4;
+
+            int actual = Solver.EuclideMethod(a, b);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Test_EuclideMethod_Argument0ReturnedAnotherArg()
+        {
+            int a = 0;
+            int b = 13;
+            int expected = 13;
+
+            int actual = Solver.EuclideMethod(a, b);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        public void Test_EuclideMethod_3ArgsPositiveNums()
+        {
+            int a = 8;
+            int b = 12;
+            int c = 44;
+            int expected = 4;
+
+            int actual = Solver.EuclideMethod(a, b, c);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Test_EuclideMethod_3ArgsNegativeNums()
+        {
+            int a = -8;
+            int b = -12;
+            int c = -44;
+            int expected = 4;
+
+            int actual = Solver.EuclideMethod(a, b, c);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Test_EuclideMethod_Two0ReturnedAnotherArg()
+        {
+            int a = 0;
+            int b = 13;
+            int c = 0;
+            int expected = 13;
+
+            int actual = Solver.EuclideMethod(a, b, c);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        public void Test_EuclideMethod_4ArgsPositiveNums()
+        {
+            int a = 8;
+            int b = 12;
+            int c = 44;
+            int d = 20;
+            int expected = 4;
+
+            int actual = Solver.EuclideMethod(a, b, c, d);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Test_EuclideMethod_4ArgsNegativeNums()
+        {
+            int a = -8;
+            int b = -12;
+            int c = -44;
+            int d = -20;
+            int expected = 4;
+
+            int actual = Solver.EuclideMethod(a, b, c, d);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Test_EuclideMethod_Three0ReturnedAnotherArg()
+        {
+            int a = 0;
+            int b = 13;
+            int c = 0;
+            int d = 0;
+            int expected = 13;
+
+            int actual = Solver.EuclideMethod(a, b, c, d);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Test_EuclideMethod_GetTime()
+        {
+
+            int a = 8;
+            int b = 12;
+            int c = 44;
+            int d = -16;
+            long time = 0;
+
+            int actual = Solver.EuclideMethod(out time, a, b);
+            System.Diagnostics.Debug.WriteLine(time + " 1");
+
+            actual = Solver.EuclideMethod(out time, a, b, c);
+            System.Diagnostics.Debug.WriteLine(time + " 2");
+
+            actual = Solver.EuclideMethod(out time, a, b, c, d);
+            System.Diagnostics.Debug.WriteLine(time + " 3");
+        }
+        #endregion
+
+        #region Test Stein's Method
+
+        [TestMethod]
+        public void Test_SteinMethod_PositiveNums()
+        {
+            int a = 8;
+            int b = 12;
+            int expected = 4;
+
+            int actual = Solver.SteinMethod(a, b);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Test_SteinMethod_NegativeNums()
+        {
+            int a = -8;
+            int b = -12;
+            int expected = 4;
+
+            int actual = Solver.SteinMethod(a, b);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Test_SteinMethod_Argument0ReturnedAnotherArg()
+        {
+            int a = 0;
+            int b = 13;
+            int expected = 13;
+
+            int actual = Solver.SteinMethod(a, b);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        public void Test_SteinMethod_3ArgsPositiveNums()
+        {
+            int a = 8;
+            int b = 12;
+            int c = 44;
+            int expected = 4;
+
+            int actual = Solver.SteinMethod(a, b, c);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Test_SteinMethod_3ArgsNegativeNums()
+        {
+            int a = -8;
+            int b = -12;
+            int c = -44;
+            int expected = 4;
+
+            int actual = Solver.SteinMethod(a, b, c);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Test_SteinMethod_Two0ReturnedAnotherArg()
+        {
+            int a = 0;
+            int b = 13;
+            int c = 0;
+            int expected = 13;
+
+            int actual = Solver.SteinMethod(a, b, c);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        public void Test_SteinMethod_4ArgsPositiveNums()
+        {
+            int a = 8;
+            int b = 12;
+            int c = 44;
+            int d = 20;
+            int expected = 4;
+
+            int actual = Solver.SteinMethod(a, b, c, d);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Test_SteinMethod_4ArgsNegativeNums()
+        {
+            int a = -8;
+            int b = -12;
+            int c = -44;
+            int d = -20;
+            int expected = 4;
+
+            int actual = Solver.SteinMethod(a, b, c, d);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Test_SteinMethod_Three0ReturnedAnotherArg()
+        {
+            int a = 0;
+            int b = 13;
+            int c = 0;
+            int d = 0;
+            int expected = 13;
+
+            int actual = Solver.SteinMethod(a, b, c, d);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Test_SteinMethod_GetTime()
+        {
+
+            int a = 8;
+            int b = 12;
+            int c = 44;
+            int d = -16;
+            long time = 0;
+
+            int actual = Solver.SteinMethod(out time, a, b);
+            System.Diagnostics.Debug.WriteLine(time + " 1");
+
+            actual = Solver.SteinMethod(out time, a, b, c);
+            System.Diagnostics.Debug.WriteLine(time + " 2");
+
+            actual = Solver.SteinMethod(out time, a, b, c, d);
+            System.Diagnostics.Debug.WriteLine(time + " 3");
+        } 
+
+        #endregion
     }
 }
